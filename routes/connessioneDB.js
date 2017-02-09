9ed416488e978494c27e368813a06e17da5aed28
Rafolo <Rafolo@172.19.62.185>
2017-02-09 11:07:48 +0100

    Passaggio da indirizzo locale a web

--- routes/connessioneDB.js
+++ routes/connessioneDB.js
@@ -8,10 +8,11 @@ var mysql = require('mysql');
 function connectionDB()
 {
     var connection = mysql.createConnection({
-        host: 'mysql://listencheck:3306/',
-        user: 'root',
-        password: '',
-        database: 'listencheck'
+        host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+        user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+        password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+        port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+        database : process.env.OPENSHIFT_APP_NAME
     })
 
     connection.connect(function(err) {
